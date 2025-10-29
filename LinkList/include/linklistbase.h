b9094afa0ca66a1d23d108a5d5debb0ee1cf3605
quangdong2001 <dong.nguyen2760@gmail.com>
2025-10-29 23:50:50 +0700

    start link list algorithm

---
+++ LinkList/include/linklistbase.h
@@ -0,0 +1,12 @@
+#ifndef LINKLISTBASE_H
+#define LINKLISTBASE_H
+#include <iostream>
+template <class U>
+    class LinkListBase
+    {
+        public:
+            LinkListBase() = default;
+            virtual ~LinkListBase() = default;
+        
+    };
+#endif
